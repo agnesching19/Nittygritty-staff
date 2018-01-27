ce75c8e66efeee6bd5150086f4f4e3198acff267
Agnes Ching <agnesching19@gmail.com>
2018-01-27 13:20:33 +0000

    added root path and generated pages controller

--- Gemfile
+++ Gemfile
@@ -1,14 +1,9 @@
 source 'https://rubygems.org'
-
-git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
-  "https://github.com/#{repo_name}.git"
-end
-
+ruby '2.5.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
-gem 'pg'
+gem 'pg', '~> 0.18'
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
@@ -18,6 +13,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker'
+gem 'nokogiri'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
---
+++ app/controllers/staffs_controller.rb
@@ -0,0 +1,50 @@
+class StaffsController < ApplicationController
+  before_action :set_staff, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @staffs = Staff.all
+  end
+
+  def create
+    @staff = Staff.new(staff_params)
+    if @staff.save
+      redirect_to @staff, notice: 'Staff was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @Staff = Staff.new
+  end
+
+  def edit
+  end
+
+  def show
+    @staff = Staff.find(params[:id])
+  end
+
+  def update
+    if @staff.update(staff_params)
+      redirect_to staff_path(@staff)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @staff.destroy
+    redirect_to staffs_path
+  end
+
+  private
+
+  def set_staff
+    @staff = Staff.find(params[:id])
+  end
+
+  def staff_params
+    params.require(:staff).permit(:name, :email, :admin)
+  end
+end
